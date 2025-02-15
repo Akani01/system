af6a140a4c2ee6e13a510b20ae64bc7e5f0c8a37
Akani01 <78940672+Akani01@users.noreply.github.com>
2025-02-15 12:32:35 -0800

    Update settings.py
    updated aws password

--- school/settings.py
+++ school/settings.py
@@ -151,6 +151,20 @@ DATABASES = {
 #aws connected online media file
 
 #aws database
+#aes configurations
+AWS_ACCESS_KEY_ID = 'AKIA3W54F3VE6XXNV444'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'tradingmacro'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = None  # Optional, makes sure no ACLs are applied
+
+
+#file overwrite
+AWS_S3_FILE_OVERWRITE = False
+# Media storage in S3
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
 
 
 # Password validation
